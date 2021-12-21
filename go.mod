fdc8eff5c00c9ba8f3dc6f049692fc65d3f5080f
zhumengdi <jurnee.zhu@ucloud.cn>
2021-12-21 11:36:12 +0800

    reflect struct

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module tool/goutils
 
 go 1.17
+
+require github.com/tealeg/xlsx v1.0.5
--- print_struct/print_struct_test.go
+++ print_struct/print_struct_test.go
@@ -1,27 +1,16 @@
 package print_struct
 
-import "testing"
-
-type People struct {
-	Name string
-	Age  int
-}
-
-func NewPeople(name string, age int) *People {
-	return &People{
-		Name: name,
-		Age:  age,
-	}
-}
-
-var p1 = NewPeople("Jone", 13)
+import (
+	"testing"
+	"tool/goutils/common"
+)
 
 func Test_PrintStructString(t *testing.T) {
-	p1_str := PrintStructString(p1)
-	t.Logf("PrintStructString==>[\n%s\n] \n ", string(p1_str))
+	p_str := PrintStructString(common.P1)
+	t.Logf("PrintStructString==>[\n%s\n] \n ", string(p_str))
 }
 
 func Test_PrintStructJson(t *testing.T) {
-	p1_str := PrintStructJson(p1)
-	t.Logf("PrintStructJson==>[\n%s\n] \n ", string(p1_str))
+	p_str := PrintStructJson(common.P1)
+	t.Logf("PrintStructJson==>[\n%s\n] \n ", string(p_str))
 }
