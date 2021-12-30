2ad759b165999a55fb6a870336debe398efcabd4
zhumengdi <jurnee.zhu@ucloud.cn>
2021-12-30 18:09:50 +0800

    PrintSpeicalString

--- data_type_convert/data_type_convert.go
+++ data_type_convert/data_type_convert.go
@@ -1,9 +1,15 @@
 package data_type_convert
 
-import "strconv"
+import (
+	"strconv"
+)
 
 // string to int
 func StringToInt(str string) int {
 	int, _ := strconv.Atoi(str)
 	return int
+}
+
+func PrintSpeicalString(str string) string {
+	return strconv.Quote(str)
 }
--- data_type_convert/data_type_convert_test.go
+++ data_type_convert/data_type_convert_test.go
@@ -6,3 +6,8 @@ func Test_StringToInt(t *testing.T) {
 	s := "123"
 	t.Log(StringToInt(s))
 }
+
+func Test_PrintSpeicalString(t *testing.T) {
+	cs1 := "abc\n123"
+	t.Log(PrintSpeicalString(cs1))
+}
