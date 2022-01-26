dc27e1d589a6f8e5c75e27388ebd5ea7e20b1cc6
zhumengdi <jurnee.zhu@ucloud.cn>
2022-01-26 13:46:40 +0800

    10进制数据转n进制

--- data_type_convert/data_type_convert.go
+++ data_type_convert/data_type_convert.go
@@ -13,3 +13,37 @@ func StringToInt(str string) int {
 func PrintSpeicalString(str string) string {
 	return strconv.Quote(str)
 }
+
+// 10 base to t base
+func BaseConvert(number, base int) int {
+	if number == 0 {
+		return 0
+	}
+	if number < 0 {
+		return -1 // 目前不支持负数
+	}
+	remainders := make([]int, 0)
+	for {
+		if number < base {
+			remainders = append(remainders, number)
+			break
+		}
+		remainders = append(remainders, number%base)
+		number = number / base
+	}
+	count := len(remainders)
+	result := 0
+	for i := 0; i < count; i++ {
+		k := count - 1 - i
+		result = result + remainders[k]*power(10, k)
+	}
+	return result
+}
+
+func power(number, times int) int {
+	result := 1
+	for i := 0; i < times; i++ {
+		result = result * number
+	}
+	return result
+}
--- data_type_convert/data_type_convert_test.go
+++ data_type_convert/data_type_convert_test.go
@@ -1,6 +1,9 @@
 package data_type_convert
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func Test_StringToInt(t *testing.T) {
 	s := "123"
@@ -10,4 +13,9 @@ func Test_StringToInt(t *testing.T) {
 func Test_PrintSpeicalString(t *testing.T) {
 	cs1 := "abc\n123"
 	t.Log(PrintSpeicalString(cs1))
+}
+
+func Test_IntToT(t *testing.T) {
+	x := BaseConvert(5, 2)
+	fmt.Println(x)
 }
