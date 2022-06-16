6d095577c914bbd9b2d6924231e0f4a6b7e3ea9c
zhumengdi <jurnee.zhu@ucloud.cn>
2022-06-16 18:43:10 +0800

    获取当天开始和结束的时间戳

--- time/get_timestamp.go
+++ time/get_timestamp.go
@@ -1,6 +1,22 @@
 package time
 
-import "time"
+import (
+	"time"
+)
+
+// 获取当天开始时间戳
+func GetStartTimeStampOfDay() int64 {
+	y, m, d := time.Now().Date()
+	startTime := time.Date(y, m, d, 0, 0, 0, 0, time.Local)
+	return startTime.Unix()
+}
+
+// 获取当天结束时间戳
+func GetEndTimeStampOfDay() int64 {
+	y, m, d := time.Now().Date()
+	endTime := time.Date(y, m, d, 23, 59, 59, 0, time.Local)
+	return endTime.Unix()
+}
 
 // 获取指定年-月月初时间戳
 func GetMonthStartTimeStamp(year int, month time.Month) int64 {
